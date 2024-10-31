a8907cd1ccd09be37f32bd5f74017d340d880739
legi <typekopo@gmail.com>
2024-10-31 10:00:33 +0100

    lenkcja

---
+++ lekcja1/zad5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int liczba1 = 0;
+	int liczba2 = 0;
+	cout << "Podaj pierwsza liczbe ";
+	cin >> liczba1;
+	cout << "Podaj druga liczbe ";
+	cin >> liczba2;
+	cout << " Suma to: " << liczba1 + liczba2;
+	cout << " Roznica to: " << liczba1 - liczba2;
+	cout << " Iloczyn to: " << liczba1 * liczba2;
+	cout << " Iloraz to: " << liczba1 / liczba2;
+	cout << " Reszta z dzielenia to: " << liczba1 % liczba2;
+}
