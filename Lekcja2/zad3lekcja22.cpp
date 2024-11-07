b0b7ba5019c14f273bfb1a283b698625cfa513bd
legi <typekopo@gmail.com>
2024-11-07 09:44:12 +0100

    lekcja3

---
+++ Lekcja2/zad3lekcja22.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int liczba;
+	cout << " Podaj liczbe" << endl;
+	cin >> liczba;
+	if(liczba % 2 == 0) cout << "Liczba jest parzysta" << endl;
+	else cout << "Liczba jest nieparzysta" << endl;
+	if(liczba % 3 == 0) cout << "Liczba jest podzielna przez 3" << endl;
+	else cout << "Liczba nie jest podzielna przez 3" << endl;
+	
+}
