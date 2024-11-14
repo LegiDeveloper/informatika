7861fc81afeb1487bd0b5a8c563ca12240261f04
legi <typekopo@gmail.com>
2024-11-14 10:14:46 +0100

    updated2

--- Lekcja2/zad1Lekcja2.cpp
+++ Lekcja2/zad1Lekcja2.cpp
@@ -5,7 +5,7 @@ int main(){
 	cin >> wynik;
 	if ((wynik >= 0) && (wynik <= 100)) {
 		
-			if(wynik < 50) cout << "pale masz, nie zdajesz i latwo ";
+	if(wynik < 50) cout << "pale masz, nie zdajesz i latwo ";
 	else if(wynik < 70) cout << "dwa masz, zdajesz ciesz sie ";
 	else if(wynik < 85) cout << "cztery masz to moze zdasz ";
 	else if(wynik < 99) cout << "pionbtekczka brawo grathulujew !!!@!111 ";
