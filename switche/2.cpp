f4e44079c48c0ad2ed8f90a4fa4ca8583cf0d0f0
legi <typekopo@gmail.com>
2024-11-25 14:47:16 +0100

    updejtyd

--- switche/2.cpp
+++ switche/2.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 int main(){
 	int temp;
 	char Jaka;
 	cout << "Podaj temperature w stopniach celsjusza" << " ";
 	cin >> temp;
-	cout << endl << "Podaj do jakiej jednostki to przeliczyc";
+	cout << endl << "Podaj do jakiej jednostki to przeliczyc (F dla fahrenhaitow lub K dla kelwinow)";
 	cin >> Jaka;
+	
+	
 	
 	switch(Jaka){
 		case 'F':
+		case 'f':
 		cout << " " << (temp * 9/5) + 32 << " fahrenhaitow";
 		break;
 		case 'K':
+		case 'k':
 		cout << " " << temp + 273.15 << " kelwinow";
 		break;
 		
 	}
 	
-	
+
+		
+	}
 	
 }
+	/*if (islower(Jaka)){
+		Jaka = toupper(Jaka);
+		cout << Jaka;*/
