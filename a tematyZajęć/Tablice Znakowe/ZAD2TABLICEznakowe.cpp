ac89ca16286773cd42550788361c30bca18b6d9d
legi <typekopo@gmail.com>
2025-03-12 14:00:57 +0100

    komentarz

---
+++ a tematyZajęć/Tablice Znakowe/ZAD2TABLICEznakowe.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char text[20];
+    char znak;
+    int liczbaWystapien;
+    cout << "Podaj tekst: ";
+    cin.getline(text, 20);
+    cout << "Podj znak do zliczenia: ";
+    cin >> znak;
+    
+    for(int i = 0; i <= 20; i++){
+	if(text[i] == znak){
+		 liczbaWystapien += 1;
+  }
+  cout << text[i];
+
+ }
+  cout << "\n Liczba wystapien znaku: " << liczbaWystapien;
+}
